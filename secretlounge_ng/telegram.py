d61aa845c7f6c3cd567f1229b2fd4819842e36e5
DakiTeriyaki <245772787+DakiTeriyaki@users.noreply.github.com>
2026-09-25 22:01:43 -0300

    Add self karma system
    Allows users to get karma automatically by sending messages.
    
    System works in the background, can be confirmed to be working
    through /info

--- secretlounge_ng/telegram.py
+++ secretlounge_ng/telegram.py
@@ -49,7 +49,7 @@ karma_cmds = None
 linked_network: Optional[dict] = None
 
 def init(config: dict, _db, _ch):
-	global bot, db, ch, message_queue, linked_network, enable_tripcode_toggle, printer_cmds, karma_cmds, allow_downvotes, enable_stickers_min_karma, stickers_min_karma, enable_sensitive_media_min_karma, sensitive_media_min_karma
+	global bot, db, ch, message_queue, linked_network, enable_tripcode_toggle, printer_cmds, karma_cmds, allow_downvotes, enable_stickers_min_karma, stickers_min_karma, enable_sensitive_media_min_karma, sensitive_media_min_karma, self_karma
 
 	if not config.get("bot_token") or ":" not in config["bot_token"]:
 		logging.error("No Telegram bot token specified")
@@ -77,6 +77,7 @@ def init(config: dict, _db, _ch):
 	stickers_min_karma = config.get("stickers_min_karma", 0)
 	enable_sensitive_media_min_karma = config.get("enable_sensitive_media_min_karma", False)
 	sensitive_media_min_karma = config.get("sensitive_media_min_karma", 0)
+	self_karma = config.get("self_karma", None)
 	if "printers_path" in config.keys():
 		printer_cmds = load_printers(config["printers_path"])
 	else:
@@ -844,6 +845,15 @@ def relay_inner(ev: TMessage, *, caption_text=None, signed=False, tripcode=False
 	# check tripcode toggle.
 	if enable_tripcode_toggle and user.toggleTripcode:
 		tripcode = True
+
+	# list to dictionary
+	karma_dict = {k: v for d in self_karma for k, v in d.items()}
+	# add karma if enabled.
+	if self_karma and ev.content_type in karma_dict:
+		c_user = UserContainer(ev.from_user)
+		k_amount = karma_dict[ev.content_type]
+		if k_amount != 0:
+			send_answer(ev, core.give_custom_self_karma(c_user, k_amount), True)
 
 	# for signed msgs: check user's forward privacy status first
 	# FIXME? this is a possible bottleneck
